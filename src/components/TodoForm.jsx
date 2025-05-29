62504a6184726c7d5251988ef836c61346dc5bf1
evandereth <evandereth@gmail.com>
2025-05-29 21:54:12 +0100

    tested some functionalities on the todoapp. it works

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,18 +1,44 @@
 import Button from "./Button"
 import "../css/todoform.css"
+import { useState } from "react"
 
-function TodoForm(){
+function TodoForm(){ 
+    const [todos, setTodos] = useState({
+        item:"",
+        status:"incomplete",
+        tags:[]
+    })
+
+    //firstly, we handle the input details
+    function handleInput(e){
+        const {name, value} = e.target
+        setTodos(prev =>{
+            return({...prev, [name]:value})
+        })
+        console.log(name, value)
+    }
+    
+
+    //this function prevents the submit from refresh on submit
+    function handleSubmit(e){
+        e.preventDefault()
+    }
+
     return(
         <>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="theTop">
                     <input 
                         type="text"
                         placeholder="Enter Todo here..."
-                        name=""
+                        name="item"
+                        value={todos.item}
+                        onChange={handleInput}
                     />
                     <select
-                        name=""
+                        name="status"
+                        value={todos.status}
+                        onChange={handleInput}
                     >
                     <option value="incomplete">Incomplete</ option>
                         <option value="ongoing">Ongoing</option>
